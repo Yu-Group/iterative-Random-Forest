63d0d66cf1832dbe4addfbeffa3e22d1bb88e5e3
Ubuntu <ubuntu@ip-172-31-20-162.us-west-2.compute.internal>
2020-03-13 06:21:57 +0000

    add causal rf

--- irf/tree/tree.py
+++ irf/tree/tree.py
@@ -60,7 +60,7 @@ DOUBLE = _tree.DOUBLE
 
 CRITERIA_CLF = {"gini": _criterion.Gini, "entropy": _criterion.Entropy}
 CRITERIA_REG = {"mse": _criterion.MSE, "friedman_mse": _criterion.FriedmanMSE,
-                "mae": _criterion.MAE}
+                "mae": _criterion.MAE, "causal": _criterion.ATE}
 
 DENSE_SPLITTERS = {"best": _splitter.BestSplitter,
                    "random": _splitter.RandomSplitter}
